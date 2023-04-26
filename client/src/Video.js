e5d84b44a97812f0b0973eaa358ab2e7f4a45f28
Logan Lampton <lampton.logan@gmail.com>
2023-04-26 17:57:39 -0400

    rendering first entry of database on Video component

--- client/src/Video.js
+++ client/src/Video.js
@@ -2,24 +2,18 @@ import { AspectRatio } from "react-aspect-ratio";
 import { useState, useEffect } from "react";
 
 function Video({ data }) {
-  // const [currentVideo, setCurrentVideo] = useState([]);
+  const [currentVideo, setCurrentVideo] = useState(0);
 
-  const singleVideo = data.map((video) => {
-    const { name, age, coolness } = video;
+  const allVideos = data.map((video) => {
+    const { id, name, age, coolness } = video;
     return (
-      <>
+      <div key={id}>
         <p>{name}</p>
         <p>{age}</p>
         <p>{coolness}</p>
-      </>
+      </div>
     );
   });
-
-  console.log(singleVideo)
-
-  // useEffect(() => {
-  //   setCurrentVideo(singleVideo);
-  // }, []);
 
   return (
     <>
@@ -27,6 +21,7 @@ function Video({ data }) {
         <div className='video-container'>
           <h2>Video</h2>
           <p>test</p>
+          {allVideos[currentVideo]}
           <AspectRatio ratio='16/9' style={{ maxWidth: "1000px" }}>
             <iframe
               src='https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com'
