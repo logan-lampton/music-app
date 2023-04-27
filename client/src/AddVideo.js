9240970e7d8d58e4a981ee6b3eaf67241957c905
Logan Lampton <lampton.logan@gmail.com>
2023-04-27 17:11:46 -0400

    set up adding songs handleChange

--- client/src/AddVideo.js
+++ client/src/AddVideo.js
@@ -1,17 +1,45 @@
-import React from "react";
+import { useState, useEffect } from "react";
+
+const initialState = {
+  name: "",
+  link: "",
+};
 
 function AddVideo() {
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = (e) => {
+    const inputName = e.target.name;
+    const inputValue = e.target.value;
+    console.log(`${inputName}:${inputValue}`);
+    setValues({ ...values, [inputName]: inputValue });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h2>Add Song</h2>
       <div>
-        <p>
-          Name: <input />
-        </p>
-        <p>
-          Link: <input />
-        </p>
+        <label>Name:</label>
+        <input
+          type='text'
+          name='name'
+          value={values.name}
+          onChange={handleChange}
+        />
+        <label>Link:</label>
+        <input
+          type='text'
+          name='link'
+          value={values.password}
+          onChange={handleChange}
+        />
       </div>
+      <button type='submit'>Submit</button>
     </form>
   );
 }
