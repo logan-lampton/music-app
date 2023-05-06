76d491ecbaae0f55f29c1e253c22f0efca324ee7
Logan Lampton <lampton.logan@gmail.com>
2023-05-06 13:22:58 -0400

    testing firebase GET request

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,13 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+// Routes
+app.get("/hello-world", (req, res) => {
+  return res.status(200).send("Hello World!");
+});
 
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
@@ -7,3 +16,6 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+// Export the api to Firebase Cloud Functions
+exports.app = functions.https.onRequest(app);
